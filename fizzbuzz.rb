313c6db2422ff10b482b2bb9a7b54fbdd22ada01
John Day <itisjohnday@gmail.com>
2017-09-10 17:32:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
 def fizzbuzz(num)
-  
+  if (num / 3 == 1)
+    return "Fizz"
+  elsif (num / 5 == 1)
+    return "Buzz"
+  elsif (num / 15 == 1)
+    return "FizzBuzz"
+  else
+    return nil
 end
