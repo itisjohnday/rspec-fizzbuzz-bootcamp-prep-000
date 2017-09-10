3a1362b6e14ae1074e6721895f1d39911165992f
John Day <itisjohnday@gmail.com>
2017-09-10 17:38:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(num)
-  if (num / 3 == 1)
+def fizzbuzz(int)
+  if (int % 3 == 0)
     return "Fizz"
-  elsif (num / 5 == 1)
+  elsif (int % 5 == 0)
     return "Buzz"
-  elsif (num / 15 == 1)
+  elsif (int % 15 == 0)
     return "FizzBuzz"
   else
     return nil
